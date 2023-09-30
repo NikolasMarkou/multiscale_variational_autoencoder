8b5dcf603639ddc2eeb3b11dcbe16fe1a9a79895
arxwn <nikolasmarkou@gmail.com>
2023-09-30 12:55:41 +0300

    [wip] building new mvae

--- mvae/train_loop.py
+++ mvae/train_loop.py
@@ -439,9 +439,16 @@ def train_loop(
                                      max_outputs=visualization_number, step=ckpt.step)
 
                     # different input scales batch
-                    for i, d in enumerate(scale_gt_image_batch):
+                    for i in range(len(denoiser_index) - 1):
                         tf.summary.image(name=f"input/scale_{i}/output", data=d * 255,
                                          max_outputs=visualization_number, step=ckpt.step)
+                    tf.summary.image(name=f"input/scale_{len(denoiser_index)-1}/output",
+                                     data=models.denormalizer(scale_gt_image_batch[-1]) / 255,
+                                     max_outputs=visualization_number,
+                                     step=ckpt.step)
+                    # for i, d in enumerate(scale_gt_image_batch):
+                    #     tf.summary.image(name=f"input/scale_{i}/output", data=d * 255,
+                    #                      max_outputs=visualization_number, step=ckpt.step)
 
                     # denoised batch
                     for i, d in enumerate(prediction_denoiser):
