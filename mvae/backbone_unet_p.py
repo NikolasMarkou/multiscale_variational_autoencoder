c931c3adddf5ff6dfa71a921d83d6946799254fd
arxwn <nikolasmarkou@gmail.com>
2023-10-05 14:34:36 +0300

    [wip] added proper blur, moved dropout

--- mvae/backbone_unet_p.py
+++ mvae/backbone_unet_p.py
@@ -277,18 +277,18 @@ def builder(
                         bn_post_params=bn_params,
                         ln_post_params=ln_params,
                         conv_params=params)
-            x = \
-                conv2d_wrapper(
-                    input_layer=x,
-                    bn_post_params=bn_params,
-                    ln_post_params=ln_params,
-                    conv_params=conv_params_res_2[i])
             if dropout_params is not None:
                 x = (
                     tf.keras.layers.Dropout(rate=dropout_params["rate"])(x))
             if dropout_2d_params is not None:
                 x = (
                     tf.keras.layers.SpatialDropout2D(rate=dropout_2d_params["rate"])(x))
+            x = \
+                conv2d_wrapper(
+                    input_layer=x,
+                    bn_post_params=bn_params,
+                    ln_post_params=ln_params,
+                    conv_params=conv_params_res_2[i])
             x = \
                 conv2d_wrapper(
                     input_layer=x,
